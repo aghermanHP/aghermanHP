08391b23471bf55bb01529123aa5a96fb0f49050
aghermanHP <agherman@pentalog.com>
2019-01-13 22:31:15 +0200

    was modified random variables maker

--- SDA/lab1.cpp
+++ SDA/lab1.cpp
@@ -6,7 +6,8 @@ struct Table
     int m;
     int i;
     int j;
-    int some=0;
+    int z;
+    int some;
     
 };
 
@@ -30,7 +31,8 @@ int main ()
     for ( A1.i = 0; A1.i < A1.n; A1.i++)
     {
         for( A1.j = 0; A1.j < A1.m; A1.j++)
-    cout << rand() %100 <<"  ";
+        A1.z = rand() %100;
+    cout <<A1.z  <<"  ";
     cout << endl;
     }
     //____________________________________________________
@@ -38,10 +40,11 @@ int main ()
     for ( A2.i = 0; A2.i < A2.n; A2.i++)
     {
         for( A2.j = 0; A2.j < A2.m; A2.j++)
-    cout << rand() %100 <<"  ";
+        A2.z = rand() %100;
+    cout << A2.z <<"  ";
     cout << endl;
     }
     for (A1.j = 0; A1.j < A1.m; A1.j++)
-    A1.some = A1.some + A1.j;
+    A1.some = A1.some + A1.z;
     cout <<A1.some;
 }
