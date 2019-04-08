22d898d4872a7513ea796b55c81c6866cf981a65
aghermanHP <agherman@pentalog.com>
2019-04-08 08:28:20 +0300

    was modified Hello Word Vue-Cli proj

--- Vuejs/src/App.vue
+++ Vuejs/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/1.png">
     <router-view/>
   </div>
 </template>
--- Vuejs/src/components/HelloWorld.vue
+++ Vuejs/src/components/HelloWorld.vue
@@ -1,82 +1,50 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Nous avons plaisi de fair cette presentation</h2>
     <ul>
       <li>
         <a
-          href="https://vuejs.org"
+          href="https://www.facebook.com/ion.guidea.98"
           target="_blank"
         >
-          Core Docs
+          Guidea Ion
         </a>
       </li>
       <li>
         <a
-          href="https://forum.vuejs.org"
+          href="https://www.facebook.com/adrian.gherman1997"
           target="_blank"
         >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
+          Gherman Adrian
         </a>
       </li>
       <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>Bibliographie</h2>
     <ul>
       <li>
         <a
-          href="http://router.vuejs.org/"
+          href="https://www.youtube.com/watch?v=cP9bhEknW_g"
           target="_blank"
         >
-          vue-router
+          Vue CLI 3 Youtube
         </a>
       </li>
       <li>
         <a
-          href="http://vuex.vuejs.org/"
+          href="https://l.facebook.com/l.php?u=https%3A%2F%2Fmedium.com%2Fcodingthesmartway-com-blog%2Fvue-js-2-quickstart-tutorial-2017-246195cfbdd2%3Ffbclid%3DIwAR1P0kEQHev435XUFCEbPqFGckZVEat6Rt0Y99azRyW945g-ejavB-Xstts&h=AT34umE0jobMgmD4IjeCliztqtXvsdFKvkUifiTph2xztuojgEh__QzeW3yWgCyMNYIs1widNrkPRJAYJco-LWORbsd61PFEXUCXEBQtztNpm_7lD2XIILhDL82LrA"
           target="_blank"
         >
-          vuex
+          comment instale
         </a>
       </li>
       <li>
         <a
-          href="http://vue-loader.vuejs.org/"
+          href="https://l.facebook.com/l.php?u=https%3A%2F%2Fraygun.com%2Fblog%2Fpopular-javascript-frameworks%2F%3Ffbclid%3DIwAR2e66RxJ_0UwISsF-fEPEeg7I-BsgAP0dUPIRhdzM2gjs9g6ZboUfePYmQ&h=AT34umE0jobMgmD4IjeCliztqtXvsdFKvkUifiTph2xztuojgEh__QzeW3yWgCyMNYIs1widNrkPRJAYJco-LWORbsd61PFEXUCXEBQtztNpm_7lD2XIILhDL82LrA"
           target="_blank"
         >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
+          9 Frameworks
         </a>
       </li>
     </ul>
@@ -88,7 +56,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Bonjour a tous'
     }
   }
 }
