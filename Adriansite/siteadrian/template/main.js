c0b53c49d16872ef4ceda80654e132afcfe319c8
aghermanHP <agherman@pentalog.com>
2019-05-20 23:14:01 +0300

    i forget to make comit and now it is a big commit

--- Adriansite/siteadrian/template/main.js
+++ Adriansite/siteadrian/template/main.js
@@ -1,19 +1,38 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("DropdownMenu").classList.toggle("show");
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('myclock').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  var y = document.forms["myForm"]["SecundName"]
+  if (x == "" || x != string){
+    alert ("Secund nume must bee with no . = - or other signes or not indroduced")
+  }
+  var z = document.forms["myForm"]["fname"].value;
+  if (z != number){
+    alert ("just numbers!!!!!!");
+    return false;
   }
 }
