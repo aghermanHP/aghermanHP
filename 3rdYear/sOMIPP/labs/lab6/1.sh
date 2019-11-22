715baa5bf71fb8563c1c3fb46f7ca04e2add0ef8
aghermanHP <adrian.gherman1@gmail.com>
2019-11-22 09:21:51 +0200

    was aded lab6 to somippp

--- 3rdYear/sOMIPP/labs/lab6/1.sh
+++ 3rdYear/sOMIPP/labs/lab6/1.sh
@@ -1,3 +1,5 @@
 # cd /home/ | mkdir test 
 
-cd /home/ && sudo mkdir test && sudo chmod 777 test && test -d "test/" && cd test && sudo echo "catalog test was created successfully" > raport && mkdir Data_Ora_Lansarii_Scriptului || echo Does not exist > ~/raport.txt && cd /home/test/
+cd /home/gherman/ &&  mkdir test || echo 'directory test allredy exist' > ~/test/raport.txt && chmod 777 test && test -d "test/" && cd test &&  echo "catalog test was created successfully" >> raport.txt;
+cd /home/gherman/test >> ~/test/raport.txt  && mkdir Data_Ora_Lansarii_Scriptului
+ping www.traiasca_moldova.md || echo erroare www.traiasca_moldova.md nu este accesibil sau nu exista >> ~/test/raport.txt
---
+++ 3rdYear/sOMIPP/labs/lab6/2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+at  now + 2 minute -f ./1.sh
+# open to see all at proces
+tail -n 0 -f ~/test/raport.txt
